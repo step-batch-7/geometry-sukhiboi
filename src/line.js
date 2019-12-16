2cda39efce8e30881d2a7a5a82e9358075d247e1
sukhiboi <41sukhdevsingh@gmail.com>
2019-12-16 15:17:00 +0530

    Deleted arePointsEqual function
    What changes you have made?
    - Deleted arePointEqual function from line.js
    
    Why this change is necessary?
    - Because, we are now using Point class for creating a line object, and
    point class has isEqualsTo method to comapre two points, that's why
    there is no need of a seprate function.
    
    What is the impact of the change?
    - Code is more readable and simpler.

--- src/line.js
+++ src/line.js
@@ -1,11 +1,5 @@
 const Point = require("./point");
 
-const arePointsEqual = function(point1, point2) {
-  const areXcoordinatesEqual = point1.x === point2.x;
-  const areYcoordinatesEqual = point1.y === point2.y;
-  return areXcoordinatesEqual && areYcoordinatesEqual;
-};
-
 class Line {
   constructor(start, end) {
     this.start = new Point(start.x, start.y);
@@ -22,8 +16,8 @@ class Line {
     if (this === otherLine) return true;
     if (!(otherLine instanceof Line)) return false;
 
-    const areStartingPointsEqual = arePointsEqual(this.start, otherLine.start);
-    const areEndingPointsEqual = arePointsEqual(this.end, otherLine.end);
+    const areStartingPointsEqual = this.start.isEqualTo(otherLine.start);
+    const areEndingPointsEqual = this.end.isEqualTo(otherLine.end);
     const areLinesEqual = areStartingPointsEqual && areEndingPointsEqual;
     return areLinesEqual;
   }
